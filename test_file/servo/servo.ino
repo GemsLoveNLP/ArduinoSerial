dcace1f433e9b3f413cfb415c23da1153b6a7606
GemsLoveNLP <6438169421@student.chula.ac.th>
2025-03-27 14:00:40 +0700

    add: servo feedback experiment

--- test_file/servo/servo.ino
+++ test_file/servo/servo.ino
@@ -8,24 +8,30 @@ float feedback = 0;
 void setup() {
   myServo.attach(servoPin);  // Attach servo
   Serial.begin(9600);        // Start serial monitor
+  delay(2000);
 }
 
 void loop() {
-  for (int angle = 0; angle <= 180; angle += 30) {
+
+  for (int angle = 0; angle <= 180; angle += 1) {
     myServo.write(angle);  // Move servo to the current angle
     delay(500);            // Wait for the servo to move
 
     int potValue = analogRead(potPin);  // Read the potentiometer value
-    feedback = 0.39078*potValue - 22.8735;
+    feedback = 0.3678*potValue - 22.8739;
     Serial.print("Set Angle: ");
     Serial.print(angle);
     Serial.print(" | Analog Read: ");
     Serial.print(potValue);
-    Serial.print(" | Feedback: ");
-    Serial.println(feedback);
+    Serial.print("| Angle Measured: ");
+    Serial.print(feedback);
+    // Serial.print(",");
+    Serial.println("");
 
-    delay(500);  // Small delay before the next step
+    delay(100);  // Small delay before the next step
   }
+  Serial.println("");
+  
 
   delay(2000);  // Pause before restarting the loop
 }
